3f1542a7aa811bb8e8d21044a479b89f9167a34d
MaEr <395155739@qq.com>
2017-07-19 18:08:34 +0800

    ui搭建
    列表

--- NTJOA/NTJOA/Controller/TJHomeController.swift
+++ NTJOA/NTJOA/Controller/TJHomeController.swift
@@ -8,15 +8,13 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "Home_Cell"
 
 class TJHomeController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.title = "首页"
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,54 +22,58 @@ class TJHomeController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    //MARK:Creat UI
+    func configureLayout() {
+        //let layout = self.collectionViewLayout
+        
+        
     }
-    */
-
+    
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TJHomeCell
     
         // Configure the cell
-    
+        
+        cell.name.text = "事务申请"
+        cell.image.image = UIImage(named: "SWSQ") //UIImage(name:"SWSQ")
+        
         return cell
     }
 
     // MARK: UICollectionViewDelegate
 
-    /*
+    
     // Uncomment this method to specify if the specified item should be highlighted during tracking
     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
         return true
     }
-    */
+    
 
-    /*
+    
     // Uncomment this method to specify if the specified item should be selected
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        
+        let main = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let applyVc = main.instantiateViewController(withIdentifier:"TJApplyListController")
+        self.navigationController?.pushViewController(applyVc, animated: true)
+        
         return true
     }
-    */
-
-    /*
+    
     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
         return false
@@ -84,6 +86,5 @@ class TJHomeController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
     }
-    */
 
 }
---
+++ NTJOA/NTJOA/View/TJHomeCell.swift
@@ -0,0 +1,16 @@
+//
+//  TJHomeCell.swift
+//  NTJOA
+//
+//  Created by chenchuang on 17/7/19.
+//  Copyright © 2017年 app. All rights reserved.
+//
+
+import UIKit
+
+class TJHomeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
